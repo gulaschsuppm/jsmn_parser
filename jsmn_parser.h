9f06fda40483511d8784cff0a71383c0f12767b2
Lichtenegger <Maximilian.Lichtenegger@zuehlke.com>
2021-03-26 11:29:51 +0100

    Added some tests and added c++ guard

--- jsmn_parser.h
+++ jsmn_parser.h
@@ -3,4 +3,164 @@
 
 #include "jsmn.h"
 
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+const jsmntok_t* tok_find_end(const jsmntok_t* tok);
+bool tok_is_key(const jsmntok_t* tok);
+bool tok_is_value(const jsmntok_t* tok);
+bool tok_string_compare(const jsmntok_t* tok, const char* js, const char* key);
+int tok_get_size(const jsmntok_t* tok);
+const jsmntok_t* tok_get_value(const jsmntok_t* tok);
+const jsmntok_t* tok_get_index(const jsmntok_t* tok, int index);
+const jsmntok_t* tok_get_key(const jsmntok_t* tok, const char* js, const char* key);
+const jsmntok_t* tok_get_leaf(const jsmntok_t* tok, const char* js, const char* key);
+
+const jsmntok_t* tok_find_end(const jsmntok_t* tok)
+{
+	const jsmntok_t* start_tok = tok;
+	// Add ourselves
+	const jsmntok_t* end_tok = ++tok;
+
+	int layers = start_tok->size;
+
+	for (int i = 0; i < layers; i++)
+	{
+		end_tok = tok_find_end(end_tok);
+	}
+
+	return end_tok;
+}
+
+bool tok_is_key(const jsmntok_t* tok)
+{
+	return (tok->type == JSMN_STRING && tok->size == 1);
+}
+
+bool tok_is_value(const jsmntok_t* tok)
+{
+	return (tok->size == 0);
+}
+
+bool tok_string_compare(const jsmntok_t* tok, const char* js, const char* key)
+{
+	bool same = false;
+	const char* start = js + tok->start;
+	const char* end = js + tok->end;
+
+	while (start != end)
+	{
+		if (*start != *key)
+		{
+			return same;
+		}
+		start++;
+		key++;
+	}
+
+	same = true;
+
+	return same;
+}
+
+int tok_get_size(const jsmntok_t* tok)
+{
+	const jsmntok_t* end = tok_find_end(tok);
+	return end - tok;
+}
+
+const jsmntok_t* tok_get_value(const jsmntok_t* tok)
+{
+	const jsmntok_t* value = tok;
+	
+	if (tok_is_key(tok))
+	{
+		++value;
+	}
+
+	return value;
+}
+
+const jsmntok_t* tok_get_index(const jsmntok_t* tok, int index)
+{
+	const jsmntok_t* tok_at_index = tok;
+	
+	// Only applicable to objects and arrays (or keys of those)
+	if (tok_get_value(tok)->type == JSMN_OBJECT || tok_get_value(tok)->type == JSMN_ARRAY)
+	{
+		if (tok_get_value(tok)->size > index)
+		{
+			// Workaround for keys
+			if (tok_is_key(tok))
+			{
+				++tok_at_index;
+			}
+			// Start at the next token
+			++tok_at_index;
+			for (int curr_index = 0; curr_index < index; curr_index++)
+			{
+				tok_at_index = tok_find_end(tok_at_index);
+			}
+		}
+	}
+
+	return tok_at_index;
+}
+
+const jsmntok_t* tok_get_key(const jsmntok_t* tok, const char* js, const char* key)
+{
+	const jsmntok_t* tok_with_key = tok;
+
+	// Only applicable to objects (and keys of objects)
+	if (tok_get_value(tok)->type == JSMN_OBJECT)
+	{
+		// Workaround for keys
+		if (tok_is_key(tok))
+		{
+			++tok_with_key;
+		}
+		// Start at the next token
+		++tok_with_key;
+		for (int curr_index = 0; curr_index < tok_get_value(tok)->size; curr_index++)
+		{
+			// Sanity check
+			if (!tok_is_key(tok_with_key))
+			{
+				return tok;
+			}
+			if (tok_string_compare(tok_with_key, js, key))
+			{
+				return tok_with_key;
+			}
+			tok_with_key = tok_find_end(tok_with_key);
+		}
+	}
+
+	return tok;
+}
+
+const jsmntok_t* tok_get_leaf(const jsmntok_t* tok, const char* js, const char* key)
+{
+	const jsmntok_t* tok_with_key = tok_get_key(tok, js, key);
+
+	// Check if key found
+	if (tok_with_key != tok)
+	{
+		const jsmntok_t* tok_with_value = tok_get_value(tok_with_key);
+		if (tok_is_value(tok_with_value))
+		{
+			return tok_with_value;
+		}
+	}
+
+	return tok;
+}
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif /* JSMN_PARSER_H */
--- tests/test_jsmn_parser.c
+++ tests/test_jsmn_parser.c
@@ -2,18 +2,249 @@
 #include "unity.h"
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-void setUp(void) {}
-void tearDown(void) {}
+static jsmn_parser parser;
 
-void test_initial()
+static jsmntok_t* tokenize(const char* js, size_t len)
 {
-    TEST_ASSERT_TRUE(0);
+    jsmntok_t* tokens = NULL;
+    uint32_t token_len = 1;
+    while (true)
+    {
+        token_len += 5;
+        tokens = realloc(tokens, sizeof(jsmntok_t) * token_len);
+		if (tokens == NULL)
+		{
+			tokens = realloc(tokens, 0);
+            return NULL;
+		}
+        int tokens_read = jsmn_parse(&parser, js, len, tokens, token_len);
+		// not enough tokens
+		if (tokens_read == JSMN_ERROR_NOMEM)
+		{
+			continue;
+		}
+		// bad token or string too short
+		else if (tokens_read == JSMN_ERROR_INVAL || tokens_read == JSMN_ERROR_PART)
+		{
+			tokens = realloc(tokens, 0);
+            return NULL;
+		}
+
+		// If we read less tokens than provided, done
+		if (tokens_read <= token_len)
+		{
+			// Fix how many tokens we actually have
+			// TODO: reallocate?
+			token_len = tokens_read;
+			break;
+		}
+    }
+
+    return tokens;
+}
+
+void setUp(void)
+{
+    jsmn_init(&parser);
+}
+
+void tearDown(void)
+{
+
+}
+
+void test_index_objects()
+{
+    const char* json_test_string =
+    "{"
+    "\"Object1\":{\"Key\":0}"
+    "\"Object2\":{\"Key\":1}"
+    "}";
+
+    jsmntok_t* tokens = tokenize(json_test_string, strlen(json_test_string));
+
+    TEST_ASSERT(tokens != NULL);
+
+    // Access the first (0) index
+    const jsmntok_t* obj1 = tok_get_index(tokens, 0);
+    TEST_ASSERT(obj1 != tokens);
+    TEST_ASSERT_EQUAL_STRING_LEN("Object1", json_test_string + obj1->start, obj1->end - obj1->start);
+
+    // Access first (0) index within dave
+    const jsmntok_t* obj1_key = tok_get_index(obj1, 0);
+    TEST_ASSERT(obj1_key != obj1);
+    TEST_ASSERT_EQUAL_STRING_LEN("Key", json_test_string + obj1_key->start, obj1_key->end - obj1_key->start);
+
+    // Access the second (1) index
+    const jsmntok_t* obj2 = tok_get_index(tokens, 1);
+    TEST_ASSERT(obj2 != tokens);
+    TEST_ASSERT_EQUAL_STRING_LEN("Object2", json_test_string + obj2->start, obj2->end - obj2->start);
+
+    const jsmntok_t* obj3 = tok_get_index(tokens, 2);
+    // Equal means nothing found
+    TEST_ASSERT(obj3 == tokens);
+}
+
+void test_index_objects_with_key()
+{
+    const char* json_test_string =
+    "\"Objects\":"
+    "{"
+    "\"Object1\":{\"Key\":0}"
+    "\"Object2\":{\"Key\":1}"
+    "}";
+
+    jsmntok_t* tokens = tokenize(json_test_string, strlen(json_test_string));
+
+    TEST_ASSERT(tokens != NULL);
+
+    // Access the first (0) index
+    const jsmntok_t* obj1 = tok_get_index(tokens, 0);
+    TEST_ASSERT(obj1 != tokens);
+    TEST_ASSERT_EQUAL_STRING_LEN("Object1", json_test_string + obj1->start, obj1->end - obj1->start);
+
+    // Access first (0) index within dave
+    const jsmntok_t* obj1_key = tok_get_index(obj1, 0);
+    TEST_ASSERT(obj1_key != obj1);
+    TEST_ASSERT_EQUAL_STRING_LEN("Key", json_test_string + obj1_key->start, obj1_key->end - obj1_key->start);
+
+    // Access the second (1) index
+    const jsmntok_t* obj2 = tok_get_index(tokens, 1);
+    TEST_ASSERT(obj2 != tokens);
+    TEST_ASSERT_EQUAL_STRING_LEN("Object2", json_test_string + obj2->start, obj2->end - obj2->start);
+
+    const jsmntok_t* obj3 = tok_get_index(tokens, 2);
+    // Equal means nothing found
+    TEST_ASSERT(obj3 == tokens);
+}
+
+void test_index_array()
+{
+    const char* json_test_string =
+    "["
+        "\"Item1\""
+        "\"Item2\""
+    "]";
+
+    jsmntok_t* tokens = tokenize(json_test_string, strlen(json_test_string));
+
+    TEST_ASSERT(tokens != NULL);
+
+    // Access the first (0) index
+    const jsmntok_t* item1 = tok_get_index(tokens, 0);
+    TEST_ASSERT(item1 != tokens);
+    TEST_ASSERT_EQUAL_STRING_LEN("Item1", json_test_string + item1->start, item1->end - item1->start);
+
+    // Access first (0) index within dave
+    const jsmntok_t* item1_key = tok_get_index(item1, 0);
+    // Equal means nothing found
+    TEST_ASSERT(item1_key == item1);
+
+    // Access the second (1) index
+    const jsmntok_t* item2 = tok_get_index(tokens, 1);
+    TEST_ASSERT(item2 != tokens);
+    TEST_ASSERT_EQUAL_STRING_LEN("Item2", json_test_string + item2->start, item2->end - item2->start);
+
+    const jsmntok_t* item3 = tok_get_index(tokens, 2);
+    // Equal means nothing found
+    TEST_ASSERT(item3 == tokens);
+}
+
+void test_index_array_with_key()
+{
+    const char* json_test_string =
+    "\"Array\":"
+    "["
+        "\"Item1\""
+        "\"Item2\""
+    "]";
+
+    jsmntok_t* tokens = tokenize(json_test_string, strlen(json_test_string));
+
+    TEST_ASSERT(tokens != NULL);
+
+    // Access the first (0) index
+    const jsmntok_t* item1 = tok_get_index(tokens, 0);
+    TEST_ASSERT(item1 != tokens);
+    TEST_ASSERT_EQUAL_STRING_LEN("Item1", json_test_string + item1->start, item1->end - item1->start);
+
+    // Access first (0) index within dave
+    const jsmntok_t* item1_key = tok_get_index(item1, 0);
+    // Equal means nothing found
+    TEST_ASSERT(item1_key == item1);
+
+    // Access the second (1) index
+    const jsmntok_t* item2 = tok_get_index(tokens, 1);
+    TEST_ASSERT(item2 != tokens);
+    TEST_ASSERT_EQUAL_STRING_LEN("Item2", json_test_string + item2->start, item2->end - item2->start);
+
+    const jsmntok_t* item3 = tok_get_index(tokens, 2);
+    // Equal means nothing found
+    TEST_ASSERT(item3 == tokens);
+}
+
+void test_index_array_of_objects()
+{
+    const char* json_test_string =
+    "["
+        "\"Object1\":"
+        "{"
+            "\"Name\": \"Dave\","
+            "\"Age\": 30"
+        "},"
+        "\"Object2\":"
+        "{"
+            "\"Size\": 5"
+        "},"
+        "\"Object3\":"
+        "{"
+            "\"City\": \"Lisbon\","
+            "\"Population\": 1534678,"
+            "\"ZIP\": 9088"
+        "}"
+    "]";
+
+    jsmntok_t* tokens = tokenize(json_test_string, strlen(json_test_string));
+
+    TEST_ASSERT(tokens != NULL);
+
+    // Access the first (0) index
+    const jsmntok_t* obj1 = tok_get_index(tokens, 0);
+    TEST_ASSERT(obj1 != tokens);
+    TEST_ASSERT_EQUAL_STRING_LEN("Object1", json_test_string + obj1->start, obj1->end - obj1->start);
+
+    // Access the second (1) index within obj1
+    const jsmntok_t* obj1_second = tok_get_index(obj1, 1);
+    TEST_ASSERT(obj1_second != obj1);
+    TEST_ASSERT_EQUAL_STRING_LEN("Age", json_test_string + obj1_second->start, obj1_second->end - obj1_second->start);
+
+    // Access the third (2) index
+    const jsmntok_t* obj3 = tok_get_index(tokens, 2);
+    TEST_ASSERT(obj3 != tokens);
+    TEST_ASSERT_EQUAL_STRING_LEN("Object3", json_test_string + obj3->start, obj3->end - obj3->start);
+
+    // Access the second (1) index within obj3
+    const jsmntok_t* obj3_second = tok_get_index(obj3, 1);
+    TEST_ASSERT(obj3_second != obj3);
+    TEST_ASSERT_EQUAL_STRING_LEN("Population", json_test_string + obj3_second->start, obj3_second->end - obj3_second->start);
+
+    // Access the fourth (3) index
+    const jsmntok_t* obj4 = tok_get_index(tokens, 3);
+    // Equal means nothing found
+    TEST_ASSERT(obj4 == tokens);
 }
 
 int main()
 {
     UNITY_BEGIN();
-    RUN_TEST(test_initial);
+    RUN_TEST(test_index_objects);
+    RUN_TEST(test_index_objects_with_key);
+    RUN_TEST(test_index_array);
+    RUN_TEST(test_index_array_with_key);
+    RUN_TEST(test_index_array_of_objects);
     return UNITY_END();
 }
