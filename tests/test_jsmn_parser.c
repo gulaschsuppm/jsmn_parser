82c8df3dc2956add1318d9be2b0639913c314518
Lichtenegger <Maximilian.Lichtenegger@zuehlke.com>
2021-03-26 10:25:03 +0100

    Setup tests

--- tests/test_jsmn_parser.c
+++ tests/test_jsmn_parser.c
@@ -1,9 +1,19 @@
 #include "jsmn_parser.h"
+#include "unity.h"
 
 #include <stdio.h>
 
+void setUp(void) {}
+void tearDown(void) {}
+
+void test_initial()
+{
+    TEST_ASSERT_TRUE(0);
+}
+
 int main()
 {
-    printf("Run tests\n");
-    return 0;
+    UNITY_BEGIN();
+    RUN_TEST(test_initial);
+    return UNITY_END();
 }
