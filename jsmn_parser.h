b2f1cb1362a942bb1b010b4d6b7b03e6bb8a7911
Lichtenegger <Maximilian.Lichtenegger@zuehlke.com>
2021-03-26 09:21:09 +0100

    Initial commit creating the cmake folder structure

---
+++ jsmn_parser.h
@@ -0,0 +1,6 @@
+#ifndef JSMN_PARSER_H
+#define JSMN_PARSER_H
+
+#include "jsmn.h"
+
+#endif /* JSMN_PARSER_H */
---
+++ tests/test_jsmn_parser.c
@@ -0,0 +1,9 @@
+#include "jsmn_parser.h"
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Run tests\n");
+    return 0;
+}
